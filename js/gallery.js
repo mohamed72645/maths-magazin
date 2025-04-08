ca8ed772dc823f45bb1d0057dfe810c6a6440028
mohamed72645 <85598010+mohamed72645@users.noreply.github.com>
2025-04-08 23:23:22 +0200

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -2,20 +2,24 @@ function openModal(imgElement) {
     var modal = document.getElementById("imageModal");
     var modalImg = document.getElementById("modalImg");
     var downloadBtn = document.getElementById("downloadBtn");
-  
+
     modal.style.display = "block";
     modalImg.src = imgElement.src;
-  
+
     // استخراج اسم الملف من الرابط
     var imageName = imgElement.src.split('/').pop();
     downloadBtn.href = imgElement.src;
     downloadBtn.setAttribute('download', imageName);
-  }
-  function closeModal() {
-    var modal = document.getElementById("imageModal");
-    modal.style.display = "none";
-  }
+}
 
+function closeModal() {
+    var modal = document.getElementById("imageModal");
+    if (modal.style.display !== "none") {  // تحقق من أن المودال مفتوح
+        modal.style.display = "none";
+    }
+}
+
+// الاستماع للضغط على زر Esc في الكيبورد
 document.addEventListener('keydown', function(event) {
   if (event.key === "Escape") {
     closeModal();
